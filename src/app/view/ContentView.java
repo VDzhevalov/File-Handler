49b8f8961e0026de77e1d4c991a9c8e7a777a4a8
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-25 12:49:19 +0300

    Add ContentView to get content data.

---
+++ src/app/view/ContentView.java
@@ -0,0 +1,9 @@
+package app.view;
+
+public class ContentView {
+    private final String fileContent = "My very important information.";
+
+    public String getContent() {
+        return fileContent;
+    }
+}
